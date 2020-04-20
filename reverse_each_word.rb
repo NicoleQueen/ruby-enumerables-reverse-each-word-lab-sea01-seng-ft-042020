6e3b6336cbb1ba7cc2924db86e225288d624ffff
QIuyan Peng <nicolepeng99@gmail.com>
2020-04-20 02:39:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@
 def reverse_each_word(a)
   index = 0 
   while index < array.length do
-    array[index].map do |element|
+    array[index].collect do |element|
       element.reverse!
       index += 1
     end
