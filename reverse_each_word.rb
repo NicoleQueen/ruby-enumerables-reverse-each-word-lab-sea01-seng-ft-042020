c778a73e2cd1e93d055d26df6f2eee8e089bbe71
QIuyan Peng <nicolepeng99@gmail.com>
2020-04-20 02:40:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 
 def reverse_each_word(a)
   index = 0 
-  while index < array.length do
-    array[index].collect do |element|
+  while index < a.length do
+    a[index].collect do |element|
       element.reverse!
       index += 1
     end
