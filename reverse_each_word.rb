19bf0c9ed415d741cd035c5adc8c2a412d0a37c9
QIuyan Peng <nicolepeng99@gmail.com>
2020-04-20 03:04:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,8 @@
 def reverse_each_word(a)
   index = 0 
   a = a.split(" ")
-  while index < a.length do
-    a[index].collect { |element| element.reverse!}
-    index += 1
-  end
+ 
+    a.collect {|element| element.reverse!}
+
   a = a.join(" ")
 end
