27aa3d51522d763672a090265d736d97f9e0aedd
QIuyan Peng <nicolepeng99@gmail.com>
2020-04-20 02:38:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
-a = "Hello there, and how are you?"
+
 def reverse_each_word(a)
   index = 0 
   while index < array.length do
-  array[index].map do |element|
-     element.reverse!
-     index += 1
+    array[index].map do |element|
+      element.reverse!
+      index += 1
+    end
   end
-end
 end
